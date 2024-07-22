568cc870c519a3c5ee824ff28e14e208f8286ebf
JaiIn <di0308@naver.com>
2024-07-23 07:52:40 +0900

    merge: app.js 성주꺼 가져옴

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./container/mainPage/MainPage";
 import KakaoPage from "./components/KakaoPage";
 import Finder from "./container/Finder/Finder";
-<<<<<<< HEAD
-import SeekerInfoPage from "./container/Finder/SeekerInfo/SeekerInfoPage";
-import Mypage from "./container/mypage/Mypage";
-=======
 import SubmitUseForm from "./container/submitPersonIfo/SubmitUseForm";
 import FindOldMan from "./container/findOldMan/FindOldMan";
->>>>>>> ac96e41a20090e9f101bc70002d44a0f4942cefe
 const App = () => {
   return (
     <Router>
@@ -18,14 +13,9 @@ const App = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/kakaoCallBack" element={<KakaoPage />} />
         <Route path="/find" element={<Finder />} />
-<<<<<<< HEAD
-        <Route path="/seekerInfo" element={<SeekerInfoPage/>}/>
-        <Route path="/myPage" element={<Mypage/>}/>
-=======
         <Route path="/signup" element={<SignUpMain />} />
         <Route path="/upload" element={<SubmitUseForm />} />
         <Route path="findOldMan" element={<FindOldMan />} />
->>>>>>> ac96e41a20090e9f101bc70002d44a0f4942cefe
       </Routes>
     </Router>
   );
