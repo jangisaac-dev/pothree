1a2950f82d018461e8db3af851fc97fa2c0d6184
jihun4452 <jihun6548@naver.com>
2024-07-22 10:23:05 +0900

    .

---
+++ backend/src/main/java/domain/UserEntity.java
@@ -0,0 +1,18 @@
+package domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class UserEntity {
+
+}
