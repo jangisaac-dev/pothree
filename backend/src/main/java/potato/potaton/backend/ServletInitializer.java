820457f1ab3abcd06b98ca2083b316b7491df86a
m2_macbook <dltkr717@gmail.com>
2024-07-22 11:38:17 +0900

    backend reinit

--- backend/src/main/java/potato/potaton/backend/ServletInitializer.java
+++ backend/src/main/java/potato/potaton/backend/ServletInitializer.java
@@ -1,13 +1,13 @@
-package helthtest.backend;
+package potato.potaton.backend;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BackendApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BackendApplication.class);
+    }
 
 }
