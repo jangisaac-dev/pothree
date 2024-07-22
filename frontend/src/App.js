207deca6ba19008fff9c83f88e47726aa5fe45c7
ppacman <smwfkim2@naver.com>
2024-07-22 17:17:13 +0900

    fix. app.jsx 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import MainPage from "./container/mainPage/MainPage";
+import MainPage from "./container/mainPage/MainPage";
 import NearBy from "./container/mainPage/components/MyNear/NearBy";
 
 const App = () => {
   return (
     <div>
-      {/* <MainPage /> */}
-      <NearBy />
+      <MainPage />
+      {/* <NearBy /> */}
     </div>
   );
 };
