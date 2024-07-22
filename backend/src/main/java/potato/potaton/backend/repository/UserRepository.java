abdb34ffce140f6f5ca8dfb843cc450205f31958
jihun4452 <jihun6548@naver.com>
2024-07-22 15:03:33 +0900

    backend reinit

--- backend/src/main/java/potato/potaton/backend/repository/UserRepository.java
+++ backend/src/main/java/potato/potaton/backend/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package potato.potaton.backend.repository;
 
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import potato.potaton.backend.domain.UserEntity;
