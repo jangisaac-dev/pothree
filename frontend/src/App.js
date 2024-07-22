09bcef2d97b98a23dd9eae086e6a540f049a723f
JaiIn <di0308@naver.com>
2024-07-23 03:13:42 +0900

    add: 병합

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./container/mainPage/MainPage";
 import KakaoPage from "./components/KakaoPage";
 import Finder from "./container/Finder/Finder";
-import SubmitUseForm from "./container/submitPersonIfo/submitInfo";
-import FindOldMan from "./container/findOldMan/FindOldMan";
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/f" element={<MainPage />} />
         <Route path="/kakaoCallBack" element={<KakaoPage />} />
         <Route path="/find" element={<Finder />} />
-        <Route path="signup" element={<SignUpMain />} />
-        <Route path="findJob" element={<SubmitUseForm />} />
-        <Route path="findOldMan" element={<FindOldMan />} />
       </Routes>
     </Router>
   );
