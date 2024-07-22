42a2d29d653c78f6222e3c8319e0242b1c0e8a5d
jihun4452 <jihun6548@naver.com>
2024-07-23 01:46:03 +0900

    회원가입 로그인, 에러코드 수정

--- backend/src/main/java/potato/potaton/backend/repository/JobSeekerRepository.java
+++ backend/src/main/java/potato/potaton/backend/repository/JobSeekerRepository.java
@@ -3,17 +3,18 @@ package potato.potaton.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import potato.potaton.backend.domain.JobSeekerEntity;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.List;
 
 public interface JobSeekerRepository extends JpaRepository<JobSeekerEntity, Long> {
+
     Optional<JobSeekerEntity> findByEmail(String email);
 
-    //이름으로 검색
-    List<JobSeekerEntity> findByNameContainingIgnoreCase(String name);
-
-    //이메일로 검색
+    // 이메일로 구직자를 조회하는 메서드 추가
     List<JobSeekerEntity> findByEmailContainingIgnoreCase(String email);
+
+    // 이름으로 구직자를 조회하는 메서드 추가
+    List<JobSeekerEntity> findByNameContainingIgnoreCase(String name);
 
     boolean existsByEmail(String email);
 }
