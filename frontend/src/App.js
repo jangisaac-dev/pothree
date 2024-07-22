5a259f8eb7bb998842796afc53064f6db48f0c6b
김효성 <ppacman@gimhyoseong-ui-MacBookPro.local>
2024-07-22 11:39:03 +0900

    feat. 불필요 파일 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Box>메인 페이지 입니다 </Box>
   );
 }
 
 export default App;
+
+
+const Box = styled.div`
+display : flex;
+width : 100px;
+height: 100px;
+background-color: aliceblue;
+
+
+
+
+`
