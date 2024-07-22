a61c094a4540865408507e84f1ed50e70089ee46
JaiIn <di0308@naver.com>
2024-07-22 16:20:00 +0900

    feat: Carosel

--- frontend/src/container/mainPage/MainPage.js
+++ frontend/src/container/mainPage/MainPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "../../components/Header";
+import Carousel from "./components/Carousel";
 const MainPage = () => {
   return (
     <Wrapper>
       <Header />
+      <Carousel/>
     </Wrapper>
   );
 };
